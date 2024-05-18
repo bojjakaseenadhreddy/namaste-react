0d6f8a2bb53b8274863d0f9cbd11b3558b600fcb
kaseenadh Reddy Bojja <kaseenadh111@gmail.com>
2024-05-17 21:18:04 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const h1Elem = React.createElement("h1", {id: "code"}, "Welcome to new Booking APP");
+const rootElem = ReactDOM.createRoot(document.getElementById("main"));
+rootElem.render(h1Elem);
+
+console.log(h1Elem)
