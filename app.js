bea485ccfa0898ec19e91fe816a534bf683c3a6e
kaseenadh Reddy Bojja <kaseenadh111@gmail.com>
2024-05-18 20:07:59 -0500

    Added packages - react, parcel, react-dom and gitignore file

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import  React from 'react';
+import  ReactDOM  from 'react-dom/client';
+
 const h1Elem = React.createElement("h1", {id: "code"}, "Welcome to new Booking APP");
 const rootElem = ReactDOM.createRoot(document.getElementById("main"));
 rootElem.render(h1Elem);
